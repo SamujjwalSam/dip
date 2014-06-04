678817290eab9e7e95ccb7e69dbdbef3cb02b757
unknown <Samujjwal_Ghosh@L-227086-P13.SYMC.SYMANTEC.COM>
2014-06-04 17:42:12 +0530

    some changes

--- chap3.py
+++ chap3.py
@@ -1,33 +1,56 @@
-import sys
+"""
+DiveIntoPython book codes ::
 
-def func (params) :
-    """take 3 arguments and prints them based on condition""" #doc_string
+My programs
+
+script options
+--------------
+--token :
+
+author(s) : Samujjwal Ghosh
+"""
+import sys
+import __builtin__
+import string
+
+
+def func(param):
+    """take 3 arguments and prints them based on condition
+    :param param:
+    """  # doc_string
     print ("in func")
-    (a,b,c) = params
-    if a == 0 :
+    (a, b, c) = param
+    if a == 0:
         print b + c
-    else :
+    else:
         print a
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     print sys.path
-    params = (0,1,2)
-    func (params)
-    params = (5,1,2)
-    print func.__doc__ #prints doc_string of the func
+    params = (0, 1, 2)
+    func(params)
+    params = (5, 1, 2)
+    print func.__doc__  # prints doc_string of the func
     
-    dict = {"a":"0","b":"1"} # dictionary, methonds: keys, values, items
-    print "dict:" + dict["b"]
-    dict["c"] = "2"
-    print dict
+    dct = {"a": "0", "b": "1"}  # dictionary, methods: keys, values, items
+    print "dict:" + dct["b"]
+    dct["c"] = "2"
+    print dct
     
-    list = ["l1","l2","l3"] # list, methods: join, insert, append
-    print [elm for elm in list] # list comprehention
-    list.insert(0,"l0")
-    print [elm for elm in list]
+    lst = ["l1", "l2", "l3"]  # list, methods: join, insert, append
+    print [elm for elm in lst]  # list comprehension
+    lst.insert(0, "l0")
+    print [elm for elm in lst]
+    print getattr(lst, 'pop')
+    print [elm for elm in lst if elm == 'l2']  # list filtering
+
+    tpl = ("t1", "t2", "t3")
+    print tpl[-1]
     
-    tuple = ("t1","t2","t3")
-    print tuple[-1]
-    
-    str = "this is a string"
-    print str.split("s")
+    st = "this is a string"
+    print st.split("s")
+    #print type(st)
+    #print dir(st)
+    #print string.punctuation
+    #print dir(string)
+    #print dir(__builtin__)
