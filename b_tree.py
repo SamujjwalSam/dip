6b5fdd6f07fc98870171fe91abc6785eb2728b55
Samujjwal_Ghosh <samujjwal86@gmail.com>
2014-06-04 20:29:28 +0530

    binary tree code started

---
+++ b_tree.py
@@ -0,0 +1,12 @@
+"""
+Implementing a Binary tree using a list type variable
+__author__ = 'Samujjwal_Ghosh'
+"""
+import stack_list
+import queue_list
+
+#todo: implement Binary tree
+
+if __name__ == "__main__":
+    print "binary tree"
+    user_input = raw_input("Enter choice; 0:quit, 1:push, 2:pop: ")
