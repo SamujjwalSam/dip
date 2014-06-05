3372421ad4ba7208d5e078ccf2213752e2101702
Samujjwal_Ghosh <samujjwal86@gmail.com>
2014-06-05 22:41:09 +0530

    some code

--- b_tree.py
+++ b_tree.py
@@ -1,44 +1,12 @@
-# coding=utf-8
 """
 Implementing a Binary tree using a list type variable
 __author__ = 'Samujjwal_Ghosh'
 """
-import sys
-
+import stack_list
 import queue_list
-
 
 #todo: implement Binary tree
 
-
-def put(q, user_input):
-    """
-
-    :param q:
-    :param user_input:
-    :return:
-    """
-    if user_input == 1:
-        getattr(queue_list, 'q_in')(q)
-    elif user_input == 2:
-        print "Removed: " + getattr(queue_list, 'q_out')(q)  # using getattr
-        # calling the method in module queue_list; alt: queue_list.q_out(q)
-    print q
-    return 1
-
-
-def exiting(q, user_input):
-    """
-
-    :param q:
-    :param user_input:
-    :return:
-    """
-    user_input and put(q, user_input) or sys.exit(1)
-
 if __name__ == "__main__":
     print "binary tree"
-    q = []
-    while 1:
-        user_input = input("Enter choice; 0:exit, 1:insert, 2:delete: ")
-        exiting(q, user_input)
+    user_input = raw_input("Enter choice; 0:quit, 1:push, 2:pop: ")
